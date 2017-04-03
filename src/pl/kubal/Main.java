8d9a739c7bf21d72da2934d2e3b691bca9f97147
Kuba Leszczynski <kuba_leszczynski@wp.pl>
2017-04-03 18:46:41 +0200

    Initial commit test

---
+++ src/pl/kubal/Main.java
@@ -0,0 +1,13 @@
+package pl.kubal;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        public int divide(int a,int b){
+            if(b==0) throw new ArithmeticException("Nie dziel przez 0");
+            return a /b ;
+            
+        }
+    }
+}
